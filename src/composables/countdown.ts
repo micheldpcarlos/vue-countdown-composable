23c37d2a8945438653ec2e7c4cdefba9c6a9c95d
Michel Carlos <mcarlos@littleotterhealth.com>
2023-04-10 16:43:30 -0300

    remove not needed code

--- src/composables/countdown.ts
+++ src/composables/countdown.ts
@@ -185,142 +185,142 @@ export function useDateCountdown() {
  * @param {number} [countInterval=1000] - Interval time in ms to check the counter
  * @returns
  */
-export function useNumberCountdown() {
-  const currentCountdownTime = ref(0);
-  const countIntervalInMs = ref<number>(1000);
-  const separatedValues = ref(false);
+// export function useNumberCountdown() {
+//   const currentCountdownTime = ref(0);
+//   const countIntervalInMs = ref<number>(1000);
+//   const separatedValues = ref(false);
 
-  // user set interval to update the returned data
-  const timeInterval = ref<number>(0);
-  const initInterval = (currentNewDate: Date) => {
-    timeInterval.value = setInterval(() => {
-      const newTime = currentCountdownTime.value - countIntervalInMs.value;
-      if (newTime <= 0) {
-        currentCountdownTime.value = 0;
-        clearInterval(timeInterval.value);
-      } else {
-        currentCountdownTime.value = newTime;
-      }
-    }, countIntervalInMs.value);
-  };
+//   // user set interval to update the returned data
+//   const timeInterval = ref<number>(0);
+//   const initInterval = (currentNewDate: Date) => {
+//     timeInterval.value = setInterval(() => {
+//       const newTime = currentCountdownTime.value - countIntervalInMs.value;
+//       if (newTime <= 0) {
+//         currentCountdownTime.value = 0;
+//         clearInterval(timeInterval.value);
+//       } else {
+//         currentCountdownTime.value = newTime;
+//       }
+//     }, countIntervalInMs.value);
+//   };
 
-  const remainingMiliseconds = computed(() => {
-    if (!separatedValues.value)
-      return intervalDurationObject.value.seconds || 0;
-    return differenceInSeconds(countdownDate.value, currentDate.value);
-  });
+//   const remainingMiliseconds = computed(() => {
+//     if (!separatedValues.value)
+//       return intervalDurationObject.value.seconds || 0;
+//     return differenceInSeconds(countdownDate.value, currentDate.value);
+//   });
 
-  const remainingSeconds = computed(() => {
-    if (!separatedValues.value)
-      return intervalDurationObject.value.seconds || 0;
-    return differenceInSeconds(countdownDate.value, currentDate.value);
-  });
+//   const remainingSeconds = computed(() => {
+//     if (!separatedValues.value)
+//       return intervalDurationObject.value.seconds || 0;
+//     return differenceInSeconds(countdownDate.value, currentDate.value);
+//   });
 
-  const remainingMinutes = computed(() => {
-    if (!separatedValues.value)
-      return intervalDurationObject.value.minutes || 0;
-    return differenceInMinutes(countdownDate.value, currentDate.value);
-  });
+//   const remainingMinutes = computed(() => {
+//     if (!separatedValues.value)
+//       return intervalDurationObject.value.minutes || 0;
+//     return differenceInMinutes(countdownDate.value, currentDate.value);
+//   });
 
-  const remainingHours = computed(() => {
-    if (!separatedValues.value) return intervalDurationObject.value.hours || 0;
-    return differenceInHours(countdownDate.value, currentDate.value);
-  });
+//   const remainingHours = computed(() => {
+//     if (!separatedValues.value) return intervalDurationObject.value.hours || 0;
+//     return differenceInHours(countdownDate.value, currentDate.value);
+//   });
 
-  const remainingDays = computed(() => {
-    if (!separatedValues.value) return intervalDurationObject.value.days || 0;
-    return differenceInDays(countdownDate.value, currentDate.value);
-  });
+//   const remainingDays = computed(() => {
+//     if (!separatedValues.value) return intervalDurationObject.value.days || 0;
+//     return differenceInDays(countdownDate.value, currentDate.value);
+//   });
 
-  const remainingMonths = computed(() => {
-    if (!separatedValues.value) return intervalDurationObject.value.months || 0;
-    return differenceInMonths(countdownDate.value, currentDate.value);
-  });
+//   const remainingMonths = computed(() => {
+//     if (!separatedValues.value) return intervalDurationObject.value.months || 0;
+//     return differenceInMonths(countdownDate.value, currentDate.value);
+//   });
 
-  const remainingYears = computed(() => {
-    if (!separatedValues.value) return intervalDurationObject.value.years || 0;
-    return differenceInYears(countdownDate.value, currentDate.value);
-  });
+//   const remainingYears = computed(() => {
+//     if (!separatedValues.value) return intervalDurationObject.value.years || 0;
+//     return differenceInYears(countdownDate.value, currentDate.value);
+//   });
 
-  /**
-   * Pause the countdown.
-   */
-  const pauseCountdown = () => {
-    clearInterval(timeInterval.value);
-  };
+//   /**
+//    * Pause the countdown.
+//    */
+//   const pauseCountdown = () => {
+//     clearInterval(timeInterval.value);
+//   };
 
-  /**
-   * Resume the countdown.
-   *
-   * DateCountdown is aways compared with current date, on resume it will jump to current difference.
-   * If you want a more controllable counter, please use the NumberCountdown
-   */
-  const resumeCountdown = () => {
-    clearInterval(timeInterval.value);
-    const currentNewDate = new Date();
-    const diffInMs = differenceInMilliseconds(
-      countdownDate.value,
-      currentNewDate
-    );
+//   /**
+//    * Resume the countdown.
+//    *
+//    * DateCountdown is aways compared with current date, on resume it will jump to current difference.
+//    * If you want a more controllable counter, please use the NumberCountdown
+//    */
+//   const resumeCountdown = () => {
+//     clearInterval(timeInterval.value);
+//     const currentNewDate = new Date();
+//     const diffInMs = differenceInMilliseconds(
+//       countdownDate.value,
+//       currentNewDate
+//     );
 
-    if (diffInMs > 0) {
-      currentDate.value = currentNewDate;
-      initInterval();
-    } else {
-      // reset to zero if not positive
-      // the user is resuming a countdown that ended
-      currentDate.value = countdownDate.value;
-    }
-  };
+//     if (diffInMs > 0) {
+//       currentDate.value = currentNewDate;
+//       initInterval();
+//     } else {
+//       // reset to zero if not positive
+//       // the user is resuming a countdown that ended
+//       currentDate.value = countdownDate.value;
+//     }
+//   };
 
-  /**
-   *
-   * @param countdownTime - Time in miliseconds to start countdown
-   * @param updateInterval - Interval time in ms to check the counter - default 1000
-   * @param separated - If set to true, each one of the counter will return the total remaining converted (e.g { seconds: 120, minutes 2 })
-   */
-  const startCountdown = (
-    countdownTime: number,
-    updateInterval: number = 1000,
-    separated: boolean = false
-  ) => {
-    // Clear a possible previsous interval before starting
-    // to make it possible to the user restart with different parameters
-    clearInterval(timeInterval.value);
+//   /**
+//    *
+//    * @param countdownTime - Time in miliseconds to start countdown
+//    * @param updateInterval - Interval time in ms to check the counter - default 1000
+//    * @param separated - If set to true, each one of the counter will return the total remaining converted (e.g { seconds: 120, minutes 2 })
+//    */
+//   const startCountdown = (
+//     countdownTime: number,
+//     updateInterval: number = 1000,
+//     separated: boolean = false
+//   ) => {
+//     // Clear a possible previsous interval before starting
+//     // to make it possible to the user restart with different parameters
+//     clearInterval(timeInterval.value);
 
-    // Check the date before starting
-    const nowDate = new Date();
-    if (countdownTime >= 0) {
-      currentCountdownTime.value = countdownTime;
-    } else {
-      // reset to zero on error
-      currentCountdownTime.value = 0;
-    }
+//     // Check the date before starting
+//     const nowDate = new Date();
+//     if (countdownTime >= 0) {
+//       currentCountdownTime.value = countdownTime;
+//     } else {
+//       // reset to zero on error
+//       currentCountdownTime.value = 0;
+//     }
 
-    if (typeof updateInterval !== "number" || updateInterval <= 0) {
-      console.warn("Invalid countInterval parameter, using default 1000ms");
-      countIntervalInMs.value = 1000;
-    } else {
-      countIntervalInMs.value = updateInterval;
-    }
+//     if (typeof updateInterval !== "number" || updateInterval <= 0) {
+//       console.warn("Invalid countInterval parameter, using default 1000ms");
+//       countIntervalInMs.value = 1000;
+//     } else {
+//       countIntervalInMs.value = updateInterval;
+//     }
 
-    // define if the returning values are calculated separated
-    separatedValues.value = !!separated;
+//     // define if the returning values are calculated separated
+//     separatedValues.value = !!separated;
 
-    initInterval();
-  };
+//     initInterval();
+//   };
 
-  // expose managed state as return value
-  return {
-    miliseconds: remainingMiliseconds,
-    seconds: remainingSeconds,
-    minutes: remainingMinutes,
-    hours: remainingHours,
-    days: remainingDays,
-    months: remainingMonths,
-    years: remainingYears,
-    start: startCountdown,
-    pause: pauseCountdown,
-    resume: resumeCountdown,
-  };
-}
+//   // expose managed state as return value
+//   return {
+//     miliseconds: remainingMiliseconds,
+//     seconds: remainingSeconds,
+//     minutes: remainingMinutes,
+//     hours: remainingHours,
+//     days: remainingDays,
+//     months: remainingMonths,
+//     years: remainingYears,
+//     start: startCountdown,
+//     pause: pauseCountdown,
+//     resume: resumeCountdown,
+//   };
+// }
